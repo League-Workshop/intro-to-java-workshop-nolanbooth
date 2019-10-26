ab9a37d8c23af527c6e912b32f0ced131843a189
League Student <league@iMac-11.attlocal.net>
2019-10-25 18:29:05 -0700

    today I did a quiz code, and I did a little bit of awesome or not
    Another GREAT day.

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,16 +2,20 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+public static void main(String[] args) {
+	
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+int number = new Random().nextInt(4) ;
 	// 3. Print out this variable
-
+System.out.println();
 	// 4. Get the user to enter something that they think is awesome
-
+String qwerty = JOptionPane.showInputDialog(null , "What is awesome to you?") ;
 	// 5. If the random number is 0
 
 	// -- tell the user whatever they entered is awesome!
@@ -28,6 +32,6 @@ public class AwesomeOrNot {
 
 	// -- write your own answer
 
-}
+}}
 
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,35 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0 ;
 		// 2.  Ask the user a question 
-		
+		String billy = JOptionPane.showInputDialog(null , "What family is the Cockatiel in?") ;
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(billy.equalsIgnoreCase("The Cockatoo Family")) {
+			score++ ;
+		 JOptionPane.showMessageDialog(null, "Remember to put a 'the' in front of all your answers, such as 'the cockatiel' for example. Good luck!");
+			JOptionPane.showMessageDialog(null, "You are correct!");
+		}
 		// 4.  if the user's answer was correct, add one to their score 
-		
+	
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		String qwerty = JOptionPane.showInputDialog(null ,"What is the smartest bird?") ;
+		if(qwerty.equalsIgnoreCase("The Crow")) {
+			score++ ;
+			JOptionPane.showMessageDialog(null, "You are correct!");
+		}
+		String bill = JOptionPane.showInputDialog(null , "What is the most popular pet bird?") ;
+		if (bill.equalsIgnoreCase("The Budgie")) {
+		score++ ;
+		JOptionPane.showMessageDialog(null , "You are correct!") ;
+		}
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		System.out.println(score);
 	}
 }
